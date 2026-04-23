3125b2d265a6a6d7ad2bccc4b719b55783a5c477
jcacevic <josephcacevic@hotmail.com>
2026-04-23 18:22:37 -0400

    project.sql

--- project.sql
+++ project.sql
@@ -137,7 +137,7 @@ INSERT INTO Game VALUES('VIL@SJ', '2026-02-28', 'SJ', 'VIL', 89, 57);
 INSERT INTO Game VALUES('SJ@SET', '2026-03-06', 'SET', 'SJ', 72, 65);
 
 INSERT INTO Game VALUES('VIL@SET', '2025-12-23', 'SET', 'VIL', 56, 64);
-INSERT INTO Game VALUES('VIL@CRE', '2026-01-07', 'CRE', 'VIL', 76, 72);
+INSERT INTO Game VALUES('CRE@VIL', '2026-01-07', 'VIL', 'CRE', 72, 76);
 INSERT INTO Game VALUES('VIL@CON', '2026-01-24', 'CON', 'VIL', 75, 67);
 INSERT INTO Game VALUES('SET@VIL', '2026-02-04', 'VIL', 'SET', 72, 60);
 INSERT INTO Game VALUES('VIL@CRE', '2026-02-14', 'CRE', 'VIL', 69, 80);
@@ -151,7 +151,7 @@ INSERT INTO Game VALUES('CRE@CON', '2026-02-18', 'CON', 'CRE', 84, 91);
 INSERT INTO Game VALUES('CON@SET', '2026-01-13', 'SET', 'CON', 64, 69);
 INSERT INTO Game VALUES('SET@CON', '2026-02-28', 'CON', 'SET', 71, 67); 
 
---Team Stats Per Game
+--Team Stats By Game
 
 --St. Johns
 INSERT INTO Team_Game_Stats VALUES('SJ', 'SJ@CRE', 33, 65, 12, 23, 12, 18, 39, 19, 7, 3, 6, 20);
@@ -166,15 +166,27 @@ INSERT INTO Team_Game_Stats VALUES('SJ', 'SJ@SET', 26, 52, 7, 20, 13, 22, 30, 16
 --Villanova
 INSERT INTO Team_Game_Stats VALUES('VIL', 'SJ@VIL', 25, 54, 11, 26, 18, 24, 28, 13, 4, 3, 14, 21);
 INSERT INTO Team_Game_Stats VALUES('VIL', 'VIL@SJ', 20, 54, 7, 23, 10, 16, 26, 11, 4, 2, 16, 18);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'VIL@SET', 22, 45, 10, 23, 10, 16, 38, 9, 4, 3, 18, 18);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'CRE@VIL', 29, 66, 6, 24, 8, 11, 30, 14, 4, 2, 6, 15);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'VIL@CON', 20, 59, 10, 26, 17, 22, 39, 14, 4, 2, 11, 21);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'SET@VIL', 23, 51, 9, 29, 17, 22, 37, 15, 7, 0, 15, 13);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'VIL@CRE', 30, 65, 6, 21, 14, 19, 41, 14, 7, 2, 7, 16);
+INSERT INTO Team_Game_Stats VALUES('VIL', 'CON@VIL', 24, 59, 6, 24, 9, 14, 24, 12, 8, 4, 10, 14);
 
 --Creighton
 INSERT INTO Team_Game_Stats VALUES('CRE', 'SJ@CRE', 23, 50, 10, 26, 17, 20, 26, 12, 4, 2, 9, 18);
 INSERT INTO Team_Game_Stats VALUES('CRE', 'CRE@SJ', 17, 53, 6, 22, 12, 18, 30, 12, 6, 3, 18, 19);
+INSERT INTO Team_Game_Stats VALUES('CRE', 'CRE@VIL', 29, 58, 7, 23, 11, 16, 39, 10, 3, 3, 8, 12);
+INSERT INTO Team_Game_Stats VALUES('CRE', 'VIL@CRE', 27, 65, 8, 27, 7, 12, 41, 16, 5, 1, 12, 15);
 
 --Seton Hall
 INSERT INTO Team_Game_Stats VALUES('SET', 'SET@SJ', 22, 58, 5, 13, 11, 20, 41, 11, 8, 7, 13, 25);
 INSERT INTO Team_Game_Stats VALUES('SET', 'SJ@SET', 23, 56, 1, 11, 18, 27, 36, 8, 5, 4, 9, 17);
+INSERT INTO Team_Game_Stats VALUES('SET', 'VIL@SET', 21, 63, 3, 13, 11, 16, 35, 10, 7, 2, 9, 15);
+INSERT INTO Team_Game_Stats VALUES('SET', 'SET@VIL', 24, 56, 2, 17, 10, 11, 27, 9, 8, 0, 10, 18);
 
 --UConn
 INSERT INTO Team_Game_Stats VALUES('CON', 'CON@SJ', 29, 53, 9, 19, 5, 12, 26, 14, 6, 6, 15, 23);
 INSERT INTO Team_Game_Stats VALUES('CON', 'SJ@CON', 28, 59, 7, 25, 9, 11, 41, 20, 6, 9, 5, 18);
+INSERT INTO Team_Game_Stats VALUES('CON', 'VIL@CON', 25, 61, 8, 22, 17, 22, 42, 17, 5, 1, 9, 18);
+INSERT INTO Team_Game_Stats VALUES('CON', 'CON@VIL', 28, 51, 6, 19, 11, 13, 37, 19, 3, 8, 14, 15);
